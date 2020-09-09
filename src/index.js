2562ea45e352ccc7b06866c6e1195f72db2e6e5c
Justin McMurdie <mcmurdiej@gmail.com>
2020-09-09 13:13:31 -0600

    working rxjs example

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import PasswordReset from "./password-reset/password-reset";
 import PasswordReset2 from "./password-reset/password-reset-2";
 import PasswordReset3 from "./password-reset/password-reset-3";
 
+import RxjsPasswordReset from './password-reset-rxjs/password-reset-rxjs'
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <PasswordReset3 />
+    {/* <PasswordReset /> */}
+    {/* <PasswordReset2 /> */}
+    {/* <PasswordReset3 /> */}
+    <RxjsPasswordReset />
   </React.StrictMode>,
   rootElement
 );
